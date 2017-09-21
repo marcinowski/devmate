3757bbd394c919e55816690b0999a6764065f5ad
Marcin <muszynskimarcin@wp.pl>
2017-09-22 00:05:07 +1000

    Navbar

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -5,7 +5,8 @@
 :contact: marcinowski007@gmail.com
 """
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 urlpatterns = [
-
+    url(r'^/', TemplateView.as_view(), name='main')
 ]

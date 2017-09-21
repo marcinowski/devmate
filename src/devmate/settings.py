0d950bbe9af5d8c8944a9b870668f6e2515f0763
Marcin <muszynskimarcin@wp.pl>
2017-09-21 23:12:25 +1000

    Base for FE app

--- src/devmate/settings.py
+++ src/devmate/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'devmate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,3 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# MY SETTINGS
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
---
+++ src/static/site/js/skeleton.js
@@ -0,0 +1,35 @@
+(function() {
+    'use strict';
+    angular
+        .module('devMateApp', ['ngMaterial', 'ngResource'])
+        .config(function($mdThemingProvider, $resourceProvider, $httpProvider) {
+            $mdThemingProvider
+                .theme('default')
+                .primaryPalette('teal');
+            $resourceProvider.defaults.stripTrailingSlashes = false;
+            $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+            $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+        })
+        .controller('AppCtrl', AppCtrl);
+            function AppCtrl($scope, $mdDialog) {
+                $scope.showErrorDialog = function(){
+                    $mdDialog.show(
+                        $mdDialog.alert()
+                            .parent(angular.element(document.body))
+                            .clickOutsideToClose(true)
+                            .title('Oops!')
+                            .textContent("Error fetching data. Please try again later")
+                            .ariaLabel('Portfolio Error')
+                            .ok('Back.')
+                            .targetEvent(ev)
+                    );
+                };
+                $scope.parseErrorData = function(error){
+                    var error_msg = [];
+                    angular.forEach(error, function(value, key) {
+                        this.push(value[0]);
+                    }, error_msg);
+                    return error_msg.join('\n');
+                };
+            }
+})();

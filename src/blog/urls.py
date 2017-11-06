ade2b0e02c0beed2288ed0a9eef1f481465bb108
Marcin <muszynskimarcin@wp.pl>
2017-11-07 00:03:30 +1100

    Clean up with blog urls

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -7,10 +7,9 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
-from .views import ArticleDetail, ArticleList
+from .views import ArticleDetail
 
 urlpatterns = [
-    url(r'^$', ArticleList.as_view(), name='main'),
     url(r'^(?P<slug>[\w-]+)$', ArticleDetail.as_view(), name='article'),
     url(r'^(?P<id>[0-9]+)$', ArticleDetail.as_view(), name='article'),
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -9,17 +9,3 @@ class ArticleDetail(DetailView):
     template_name = 'blog/article.html'
     lookup_field = 'slug'
     context_object_name = 'article'
-
-
-class ArticleList(ListView):
-    queryset = Article.objects.get_published()
-    template_name = 'blog/main.html'
-    context_object_name = 'articles'
-    paginate_by = 10
-    ordering = 'publish_date'
-
-    def get_queryset(self):
-        q = super().get_queryset()
-        f = ArticleForm(**self.request.GET)
-        f.full_clean()
-        return q.filter(**f.data)
